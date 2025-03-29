b7cf0f982af1fa9c37ad7247c87cf57f1535e864
Rushendar-Reddy <yasanirushendarreddy@gmail.com>
2025-03-29 12:26:25 +0530

    Add files via upload

---
+++ CommandLine2.java
@@ -0,0 +1,9 @@
+class CommandLine1{
+public static void main(String args[]){
+String name=args[0];
+int num=Integer.parseInt(args[1]);
+System.out.println("Name "+name);
+System.out.println("age "+num);
+}}
+
+
