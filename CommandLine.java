edd7fe28163b498b2a01388c249bbbd2f0f3b5ff
Rushendar-Reddy <yasanirushendarreddy@gmail.com>
2025-03-29 12:02:16 +0530

    Add files via upload

---
+++ CommandLine.java
@@ -0,0 +1,14 @@
+class CommandLine{
+public static void main(String args[]){
+int n1=Integer.parseInt(args[0]);
+int n2=Integer.parseInt(args[1]);
+int a=n1+n2;
+int b=n1-n2;
+int c=n1*n2;
+int d=n1/n2;
+System.out.println("Add "+a);
+System.out.println("Sub "+b);
+System.out.println("Multi "+c);
+System.out.println("Div "+d);
+}}
+
