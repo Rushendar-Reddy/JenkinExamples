cda50a31b7102a9dba45d713c8f90bc9dd7989c4
Rushendar-Reddy <yasanirushendarreddy@gmail.com>
2025-07-11 12:26:32 +0530

    Add files via upload

---
+++ SumandAvg.java
@@ -0,0 +1,11 @@
+class SumandAvg{
+public static void main(String args[]){
+int sum=0;
+float avg;
+for (int i=0;i<=10;i++){
+sum= sum+i;
+}
+avg=sum/10;
+System.out.println("sum  of the first 10 numbers are",+sum);
+System.out.println("avg of the first  10 numbers are",+avg);
+}}
