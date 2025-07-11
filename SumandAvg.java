d8da3afbab68de0da993f3e5e805a0a60b4ebbac
Rushendar-Reddy <yasanirushendarreddy@gmail.com>
2025-07-11 12:33:34 +0530

    Update SumandAvg.java

--- SumandAvg.java
+++ SumandAvg.java
@@ -1,11 +1,15 @@
-class SumandAvg{
-public static void main(String args[]){
-int sum=0;
-float avg;
-for (int i=0;i<=10;i++){
-sum= sum+i;
+class SumandAvg {
+    public static void main(String args[]) {
+        int sum = 0;
+        float avg;
+        
+        for (int i = 0; i <= 10; i++) {
+            sum = sum + i;
+        }
+
+        avg = (float) sum / 10;
+
+        System.out.println("Sum of the first 10 numbers is: " + sum);
+        System.out.println("Average of the first 10 numbers is: " + avg);
+    }
 }
-avg=sum/10;
-System.out.println("sum  of the first 10 numbers are",+sum);
-System.out.println("avg of the first  10 numbers are",+avg);
-}}
